93d9fd49295b5be37783cef7cc9d2750c190bd9b
Baozhu Zuo <zuobaozhu@gmail.com>
2020-04-11 13:00:23 +0800

    add uninstall command

--- aip/install.py
+++ aip/install.py
@@ -1,23 +1,23 @@
 
 # The MIT License (MIT)
-# 
+#
 # Author: Baozhu Zuo (zuobaozhu@gmail.com)
-# 
+#
 # Copyright (C) 2020  Seeed Technology Co.,Ltd.
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
@@ -26,8 +26,7 @@
 
 from pip._internal.cli.base_command import Command
 from pip._internal.cli.req_command import RequirementCommand
-from pip._internal.cli.status_codes import SUCCESS,ERROR
-from pip._internal.utils import appdirs
+from pip._internal.cli.status_codes import SUCCESS, ERROR
 from pip._internal.cli import cmdoptions
 from pip._internal.network.download import Downloader
 from pip._internal.models.link import Link
@@ -41,6 +40,10 @@ from pip._internal.operations.prepare import (
 import os
 from pygit2 import clone_repository
 from pygit2 import Repository
+from .variable import *
+import shutil
+from pathlib import Path
+
 
 class installCommand(RequirementCommand):
     """
@@ -57,14 +60,13 @@ class installCommand(RequirementCommand):
     def __init__(self, *args, **kw):
         super(installCommand, self).__init__(*args, **kw)
         self.cmd_opts.add_option(
-            '-a', '--ailes',
-            dest='files',
+            '-r', '--ailes',
+            dest='uninstall',
             action='store_true',
             default=False,
-            help='Show the full list of installed files for each package.')
+            help='Install the aip package')
 
         self.parser.insert_option_group(0, self.cmd_opts)
-        self.user_data_dir = appdirs.user_data_dir(appname = "aip")
 
         index_opts = cmdoptions.make_option_group(
             cmdoptions.index_group,
@@ -73,15 +75,23 @@ class installCommand(RequirementCommand):
 
         self.parser.insert_option_group(0, index_opts)
 
-
     def run(self, options, args):
-        moduledir = self.user_data_dir + "/modules"
-        for package in args:
-            print(package)
-            clone_repository("https://github.com/"+package, moduledir + package[package.find("/") : ])
-            print(moduledir + package[package.find("/") : ]+'/.git')
-            repo = Repository(moduledir + package[package.find("/") : ]+'/.git')
-            repo.init_submodules()
-            repo.update_submodules()            
-        
-
+        moduledir = Path(user_data_dir, "modules")
+        if options.uninstall == True:
+            for package in args:
+                print(package[package.find("/")+1:])
+                shutil.rmtree(Path(moduledir, package[package.find("/")+1:]))
+        else:
+            for package in args:
+                print(package)
+                if os.path.exists(Path(moduledir, package[package.find("/") + 1:])):
+                    shutil.rmtree(
+                        Path(moduledir, package[package.find("/")+1:]))
+                clone_repository("https://github.com/"+package,
+                                 Path(moduledir, package[package.find("/")+1:]))
+                repo = Repository(
+                    Path(
+                        Path(moduledir, package[package.find("/") + 1:]), '.git')
+                )
+                repo.init_submodules()
+                repo.update_submodules()
